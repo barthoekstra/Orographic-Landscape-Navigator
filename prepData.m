28f9fac2e5cb650a85d7ec83156e941b8555be2b
Bart Hoekstra <barthoekstra@gmail.com>
2017-06-01 13:42:24 +0200

    Include daterange filenaming and improve classifier paths generation

--- prepData.m
+++ prepData.m
@@ -13,6 +13,17 @@ startdatetime  = '2015-05-01 00:00:00';
 enddatetime    = '2015-07-15 23:59:59';
 datetimeformat = 'yyyy-mm-dd HH:MM:SS';
 
+% Filenaming conventions
+% Files are named with their corresponding year or year-range, finer
+% naming could be set below
+startyear = datestr(datenum(startdatetime, datetimeformat), 'yyyy');
+endyear = datestr(datenum(enddatetime, datetimeformat), 'yyyy');
+if strcmp(startyear, endyear)
+    daterange = startyear;
+else
+    daterange = [startyear, '_', endyear];
+end
+
 % We only want high-resolution data, so we start by downloading
 % accelerometer data, and subsequently download the corresponding tracker
 % data only for those trackers which have provided a sufficiently large
@@ -30,43 +41,57 @@ lbbg_min_obs = 0; % Too low data resolution, so we accept everything
 %% Fetch Accelerometer data
 highres_hg_devices = prepBirdAccelerometerData(db_user, db_pass, ...
     trackers_hg, startdatetime, enddatetime, hg_min_obs, ...
-    'data/ResearchAreaNH.shp', 'data/unclassified');
+    'data/ResearchAreaNH.shp', ['data/unclassified/', daterange]);
 
 highres_lbbg_devices = prepBirdAccelerometerData(db_user, db_pass, ...
     trackers_lbbg, startdatetime, enddatetime, lbbg_min_obs, ...
-    'data/ResearchAreaNH.shp', 'data/unclassified');
+    'data/ResearchAreaNH.shp', ['data/unclassified/', daterange]);
 
-save('proj_settings.mat', 'db_pass', 'db_user', 'devices', ...
+save(['proj_settings_', daterange, '.mat'], 'db_pass', 'db_user', 'devices', ...
      'enddatetime', 'hg_min_obs', 'highres_hg_devices', ...
      'highres_lbbg_devices', 'lbbg_min_obs', 'startdatetime', ...
-     'trackers_hg', 'trackers_lbbg');
+     'trackers_hg', 'trackers_lbbg', 'daterange');
  
 %% Prepare accelerometer data files for classification
 %  In order to classify based on the accelerometer, do the following:
-%  1. Copy the path from paths.txt (see below) over to the right line in the
-%     settings.properties file, behind the line that starts with
-%     'unannotated_measurement_source_paths'. Make sure there are no (real)
-%     linebreaks!
-%  2. Copy the folder structure over to the data/ folder of the
+%  1. Copy the folder structure over to the data/ folder of the
 %     classification, so the folder should contain a list of
 %     numbered-folders (corresponding with device IDs with a bunch of .mat
-%     files in them.
-%
-% @NOTE: The following requires some cleanup. Make sure the directory
-% structure introduced in prepBirdAccelerometerData is changed
-% correspondingly. Unfortunately the classifier cannot handle files
-% organized in folders.
-matfiles = dir(fullfile('data/unclassified/', '*/*.mat'))
+%     files in them. @NOTE: Make sure code is NOT organized in folders
+%     anymore
+%  2. Copy the paths from paths.txt over to the settings.properties file of
+%     classifier. All lines are commented out by default. The
+%     fastest/easiest way to do the classification is by uncommenting these
+%     lines one by one and running the classifier on each one of these
+%     lines. Every time a classifications.csv file is created, you can
+%     rename these to the respective device ID. So after classification,
+%     you end up with files such as 317.csv, 6202.csv, etc.
+
+matfiles = dir(fullfile(['data/unclassified/', daterange, '/'], '*.mat'));
 
 filenames = {matfiles(:).name}';
-%folders   = {matfiles(:).folder}';
 
-%paths = strcat(folders, '/', filenames);
-%paths = replace(paths, [pwd, '/data/unclassified/'],'');
+device_ids = strtok(filenames, 'd'); % d is the delimiter between ID and yyyymmdd
+unique_ids = unique(device_ids);
+n = numel(unique_ids);
 
-pathsstring = join(filenames, ',')
-fileID = fopen('data/unclassified/paths.txt', 'w');
-fprintf(fileID, '%s', pathsstring);
+% Now build these into classifier-compatible strings
+lines = {};
+
+for i = 1:n
+    % Select all filenames with a similar ID prefix
+    current_id = unique_ids(i);
+    selector = strcmp(device_ids, current_id);
+    fileselection = filenames(selector);
+    
+    % Now transform that in a string
+    paths = join(fileselection, ',');
+    space = {' = '};
+    lines{i,1} = strcat('# unannotated_measurement_source_paths', space, paths);
+end
+
+fileID = fopen(['data/unclassified/', daterange, '/paths.txt'], 'w');
+fprintf(fileID, '%s \n', lines{:});
 fclose(fileID);
 
  
@@ -76,7 +101,7 @@ fclose(fileID);
 %  the tracker data from these moments
 trackers = [highres_hg_devices, highres_lbbg_devices];
 tracks = prepBirdData(db_user, db_pass, trackers, startdatetime, enddatetime, 'data/ResearchAreaNH.shp');
-save('proj_tracks.mat', 'tracks');
+save(['proj_tracks_', daterange, '.mat'], 'tracks');
 
 %% Fetch weather (wind) data
 %  In order to calculate orographic lift, we need wind data for all
@@ -87,10 +112,10 @@ conversionformat = 'yyyymmdd';
 startdate = str2double(datestr(startdatenum, conversionformat));
 enddate   = str2double(datestr(enddatenum, conversionformat));
 
-[wind, stations] = prepWeatherData('data/weather/', 'data/ResearchAreaNH.shp', startdate, enddate);
+[wind, stations] = prepWeatherData(['data/weather/', daterange, '/'], 'data/ResearchAreaNH.shp', startdate, enddate);
 wind = table(wind(:,1), wind(:,2), wind(:,3), wind(:,4), wind(:,5), wind(:,6), wind(:,7), ...
              'VariableNames', {'stationID', 'date', 'hour', 'wdir', 'wspeed_hr', 'wspeed_10min', 'wspeed_peak'});
-save('proj_wind.mat', 'wind', 'stations');
+save(['proj_wind_', daterange, '.mat'], 'wind', 'stations');
 
 %% Load DEM info from files
 %  For the sake of simplicity, we are not going to seperately download the
@@ -100,4 +125,4 @@ save('proj_wind.mat', 'wind', 'stations');
 %  @NOTE: Rewrite gdal and ogr2ogr scripts to a Python version not
 %  dependent on dependencies provided by MATLAB.
 dems = loadDEMinfo('data/dem/', '.wgs84.tif', -1e10);
-save('proj_dems.mat', 'dems');
+save(['proj_dems_', daterange, '.mat'], 'dems');
